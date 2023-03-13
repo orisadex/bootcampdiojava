4856cd35f34802ac2871960f203855995a66f07c
Willian Pereira <willianpereiravolks@gmail.com>
2023-03-13 11:50:40 -0300

    Aula de metodo

---
+++ trilha-java-basico/operadores/src/Operadores.java
@@ -0,0 +1,23 @@
+public class Operadores {
+    public static void main(String[] args) {
+        //x repetição
+        /*
+        int numero = 5;
+        numero++;
+        //operação na saída
+        System.out.println(-- numero);*/
+
+        /*
+        boolean variavel = false;
+        //vai mudar o seu valo com "!"
+        System.out.println(!variavel);
+        */
+
+        int a, b;
+        a = 5;
+        b = 6;
+
+        String resultado = a==b ? "Verdadeiro" : "Falso";
+        System.out.println(resultado);
+    }
+}
---
+++ trilha-java-basico/tipo-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,16 @@
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        double salarioMinimo = 2500.50;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+
+        final double VALOR_DO_PI = 3.14;
+
+        System.out.println(VALOR_DO_PI);
+    }
+}
